d4e0c8325e7d63bd94e670e9dec018b9f6b53afc
Christine Myers Keitt <cmyerskeitt@gmail.com>
2020-07-24 18:32:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,12 +2,13 @@ class Song
   attr_accessor :name, :artist, :genre 
   
   @@count= 0 
-  @@artists=0 
+  @@artists= []
   
   def initialize(name, artist, genre)
     @name= name 
     @artist= artist 
     @genre= genre
+    @@artist=
     @@count+= 1
   end 
   
